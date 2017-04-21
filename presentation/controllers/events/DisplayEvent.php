0f1b51a578910ff31e1f99b0c7a974feba8255c7
Brent Christensen <brent@eventespresso.com>
2017-04-21 11:44:09 -0700

    get event from global post

--- presentation/controllers/events/DisplayEvent.php
+++ presentation/controllers/events/DisplayEvent.php
@@ -7,6 +7,7 @@ use EE_Error;
 use EE_Event;
 use EspressoRouter\presentation\controllers\BaseController;
 use EventEspresso\core\exceptions\InvalidDataTypeException;
+use WP_Post;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -30,10 +31,20 @@ class DisplayEvent extends BaseController
      */
     public function execute()
     {
-        // ?ee_route=event_details&event_id=59
-        $event = $this->view_model->getModel()->get_one_by_ID(
-            $this->request->get('event_id')
-        );
+        global $post;
+        if (
+            $post instanceof WP_Post
+            && $post->post_type === 'espresso_events'
+            && $post->EE_Event instanceof EE_Event
+        ) {
+            $event = $post->EE_Event;
+        } else {
+            $event_id = $this->request->get('event_id');
+            if (! $event_id) {
+                return;
+            }
+            $event = $this->view_model->getModel()->get_one_by_ID($event_id);
+        }
         if(!$event instanceof EE_Event) {
             return;
         }
